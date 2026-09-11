d3f862025a70be4b6030435c0665b89502717c3e
golCMKL <tjianwa@cmkl.ac.th>
2026-09-11 20:59:42 +0700

    Create Main Flask API

---
+++ app/main.py
@@ -0,0 +1,47 @@
+import joblib
+import numpy as np
+from flask import Flask, jsonify, request
+from PIL import Image
+
+app = Flask(__name__)
+
+model = joblib.load("mlp_model.pkl")
+
+print("MLP model loaded successfully")
+
+
+@app.route('/predict', methods=["POST"])
+def predict(): 
+    if "image" not in request.files:
+        return jsonify({
+            "error: No Image provided"
+        }), 400
+    file = request.files["image"]
+
+    image = Image.open(file).convert("L")
+
+    image = image.resize((28, 28))
+
+    image = np.array(image).astype("float32")
+
+    image = image /255.0
+
+    image = image.reshape(1,784)
+
+    prediction = model.predict(image)[0]
+
+    probabilities = model.predict_proba(image)[0]
+
+    return jsonify({
+        "prediction": int(prediction),
+        "confidence": float(probabilities[prediction])
+    })
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "MLP prediction API is running"
+    })
+
+if __name__ == '__main__':
+    app.run()
